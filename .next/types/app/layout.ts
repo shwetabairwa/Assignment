01c17b733fc40368748dd0aae90fb5f2f3da47fb
Shweta <shwetabairwa027@gmail.com>
2025-09-02 19:23:07 +0530

    file sumbmit

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/shwetabairwa/Desktop/Assignment/src/app/layout.tsx
+// File: /Users/shwetabairwa/Desktop/clean-repo/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/shwetabairwa/Desktop/Assignment/src/app/page.tsx
+// File: /Users/shwetabairwa/Desktop/clean-repo/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
